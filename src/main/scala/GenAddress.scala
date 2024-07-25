02bfa02ed9cca27ecdf499f5171d7b99e43e9a16
pure-blue0 <dual__luo@outlook.com>
2024-07-25 20:31:17 +0800

    Verification completed:GenAddress

--- src/main/scala/GenAddress.scala
+++ src/main/scala/GenAddress.scala
@@ -1,69 +1,39 @@
 import chisel3._
-import chisel3.util._
 
-//class GenAddress(val FetchByteWidth: Int, val iCacheAlignByte: Int) extends Module {
-//  val io = IO(new Bundle {
-//    val sendsuccess = Input(Bool())
-//    val PcRegisterout_valid = Input(Bool())
-//    val Decode_redirectTag = Input(UInt(32.W))
-//    val Decode_redirect_valid = Input(Bool())
-//    val IEW_redirectTag = Input(UInt(32.W))
-//    val IEW_redirect_valid = Input(Bool())
-//    val commit_redirectTag = Input(UInt(32.W))
-//    val commit_redirect_valid = Input(Bool())
-//    val PcRegister_in = Output(UInt(32.W))
-//  })
-//
-//  // Default next PC is current PC + 4, aligned to iCacheAlignByte
-//  val defaultNextPC = (io.PcRegister_in + FetchByteWidth.U) & ~(iCacheAlignByte.U - 1.U)
-//
-//  // Check for redirects and prioritize them
-//  val redirectAddress = MuxCase(defaultNextPC, Seq(
-//    io.commit_redirect_valid -> io.commit_redirectTag,
-//    io.IEW_redirect_valid -> io.IEW_redirectTag,
-//    io.Decode_redirect_valid -> io.Decode_redirectTag
-//  ))
-//
-//  // Set next fetch address
-//  io.PcRegister_in := Mux(io.sendsuccess, redirectAddress, defaultNextPC)
-//}
-import chisel3._
-import chisel3.util._
-
-import chisel3._
-import chisel3.util._
-
-class GenAddress(val FetchByteWidth: Int, val iCacheAlignByte: Int) extends Module {
+class GenAddress(FetchByteWidth: Int, ICacheAlignByte: Int) extends Module {
   val io = IO(new Bundle {
-    val sendSuccess = Input(Bool())
-    val PcRegisteroutVaild = Input(Bool())
-    val PcRegisterout = Input(UInt(32.W))
-    val DecodeRedirectTag = Input(UInt(32.W))
-    val DecodeRedirectVaild = Input(Bool())
-    val IEWRedirectTag = Input(UInt(32.W))
-    val IEWRedirectVaild = Input(Bool())
-    val CommitRedirectTag = Input(UInt(32.W))
-    val CommitRedirectVaild = Input(Bool())
-    val PcRegisterIn = Output(UInt(32.W))
+    val SendSuccess = Input(Bool())
+    val pcregister_out = Input(UInt(32.W))
+    val PCRegOut_valid = Input(Bool())
+    val decode_redirect_target = Input(UInt(32.W))
+    val decode_redirect_valid = Input(Bool())
+    val IEW_redirect_target = Input(UInt(32.W))
+    val IEW_redirect_valid = Input(Bool())
+    val commit_redirect_target = Input(UInt(32.W))
+    val commit_redirect_valid = Input(Bool())
+    val pcregister_in = Output(UInt(32.W))
   })
 
-  // Priority logic
-  when(io.CommitRedirectVaild) {
-    io.PcRegisterIn := io.CommitRedirectTag
-  }.elsewhen(io.IEWRedirectVaild) {
-    io.PcRegisterIn := io.IEWRedirectTag
-  }.elsewhen(io.DecodeRedirectVaild) {
-    io.PcRegisterIn := io.DecodeRedirectTag
-  }.elsewhen(io.sendSuccess) {
-    val alignedPC = (io.PcRegisterout & ~(iCacheAlignByte.U - 1.U)) + FetchByteWidth.U
-    io.PcRegisterIn := alignedPC
-  }.otherwise {
-    io.PcRegisterIn := io.PcRegisterout
+  // 默认输出为上一周期的PC值
+  io.pcregister_in := io.pcregister_out
+
+  // 根据SendSuccess和PCRegOut_valid的状态调整PC地址
+  when(io.commit_redirect_valid) {
+    io.pcregister_in := io.commit_redirect_target
+  } .elsewhen(io.IEW_redirect_valid) {
+    io.pcregister_in := io.IEW_redirect_target
+  } .elsewhen(io.decode_redirect_valid) {
+    io.pcregister_in := io.decode_redirect_target
+  } .elsewhen(io.SendSuccess) {
+    // 对齐到ICacheAlignByte，然后加上FetchByteWidth
+    io.pcregister_in := ((io.pcregister_out & (~((ICacheAlignByte - 1).U(32.W)))) + FetchByteWidth.U(32.W))
+  } .elsewhen(io.PCRegOut_valid) {
+    io.pcregister_in := io.pcregister_out
   }
 }
-
 
 
 object VerilogGen extends App {
   println((new chisel3.stage.ChiselStage).emitVerilog(new GenAddress(8,8)))
 }
+
